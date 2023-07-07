07a34800b6f6d8558f8cfc9a4333930145bd2a96
hieuNP <ryan.nguyen@genestory.ai>
2023-07-07 09:15:21 +0700

    add gitignore

---
+++ mapping_columns.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import requests
+import json
+# Read the Excel file
+df = pd.read_excel('kit-list-swap.xlsx')
+
+# # Extract a specific column into a list
+# old_barcode_lst = df['Barcode cũ'].tolist()
+# old_barcode_lst = [ str(barcode) if len(str(barcode)) >= 12 else '0'+str(barcode) for barcode in old_barcode_lst]
+# new_barcode_lst = df['Barcode mới'].tolist()
+# new_barcode_lst = [ str(barcode) if len(str(barcode)) >= 12 else '0'+str(barcode) for barcode in new_barcode_lst]
+
+# # Print the list
+# print(old_barcode_lst)
+# print(new_barcode_lst)
+
+# Convert each row of the DataFrame into a dictionary
+data = df.to_dict(orient='records')
+
+rows = []
+# Print each dictionary representing a row
+for row in data:
+    # print(row)
+    rows.append(row)
+
+
+file_path = 'response.json'
+with open(file_path, 'r') as json_file:
+    # Read the file content
+    list_of_dicts = json.load(json_file)
+
+print('list_of_dicts: ',type(list_of_dicts))
+# Print the list of dictionaries
+mapping_barcode_n_lid = []
+for dictionary in list_of_dicts["data"]:
+    for row in rows:
+        temp_barcode = str(row['Barcode cũ']) if len(str(row['Barcode cũ'])) >= 12 else '0'+str(row['Barcode cũ'])
+        if temp_barcode == str(dictionary['barcode']):
+            entry = dict()
+            entry['old_barcode']=dictionary['barcode']
+            entry['lid']=dictionary['lid']
+            entry['new_barcode']=str(row['Barcode mới'])
+            mapping_barcode_n_lid.append(entry)
+            # print(entry)
+            query = print(f"UPDATE lab_sample SET barcode='{entry['new_barcode']}' WHERE lid='{entry['lid']}'; ")
+    
+
+
