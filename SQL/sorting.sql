16ffdcf27c604a55566ec4ce688bd365b5c5f2c4
hieuNP <ryan.nguyen@genestory.ai>
2023-07-07 09:17:35 +0700

    sort using concat

--- SQL/sorting.sql
+++ SQL/sorting.sql
@@ -6,3 +6,13 @@ from
     order by REVERSE(well_position) asc;
 
 --- A1 B1 C1 D1 .. H1 A2 B2 C2 D2
+
+
+select
+    smp.well_position,
+    smp.gender_pass as estimated_gender,
+from 
+    sample_mapping
+    order by CONCAT(LPAD(RIGHT(smp.well_position,-1),2,'0'),LEFT(smp.well_position,1)) asc;
+
+--- 01A 01B 01C 01D ... 02A 02B 02C ........ 12F 12G 12H
